afa3c7f8dc1b44f7398789ef6842526e2f9050c1
Pe-te-r <mburupeter8526@gmail.com>
2024-12-27 22:54:25 +0300

    finished trial1.py

--- first_app/trial1.py
+++ first_app/trial1.py
@@ -1,8 +1,39 @@
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import  ttk
+import ttkbootstrap as ttk
 
+def button_press():
+          # print(entry.get())
+          miles_input=entry_int.get()
+          km_output = miles_input * 1.61
+          text_string.set(f'{km_output} kms')
+
+# window
 window=tk.Tk()
 window.title('first app')
-window.geometry('800x700')
+window.geometry('300x200')
+
+# widgets
+text_label = ttk.Label(master=window,text='Miles to kilometers')
+text_label.pack()
+
+# frame
+frame=ttk.Frame(master=window)
+
+entry_int=tk.IntVar()
+entry=ttk.Entry(master=window,textvariable=entry_int)
+
+button=ttk.Button(window,text='convert',command=button_press)
+entry.pack(pady=10)
+button.pack()
+frame.pack( pady=10)
+
+# output
+text_string=tk.StringVar()
+text_string.set("Output show:")
+output_show = ttk.Label(master=window,text='To show output',textvariable=text_string)
+output_show.pack(pady=10)
+
+
 
 window.mainloop()
